e9a5180a96c4a3fa68fcf2c2c1ca2c4e79fbe24e
Metrax <metrax@metrax.eu>
2012-04-05 17:55:41 +0200

    metrax update

--- world.php
+++ world.php
@@ -3,12 +3,10 @@
 for ($i=0; $i<100; $i++)
   echo "Hello World!";
 
-<<<<<<< HEAD
 if(1) {
 	echo "oc test"
 }
-=======
+
 $LeifsVariable = 5;
->>>>>>> f552e64fb1fb55143defca535c0aa49c6b3ac13c
 
 ?>
